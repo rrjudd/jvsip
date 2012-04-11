1ab4470162f1ca1512e6e533e1d786b68b9dd76e
Randall Judd <rrjudd@me.com>
2012-04-11 09:33:52 -0700

    Modified gen_atest.sh so that null data pointers (NDPTR) are defined before the tests are included. Otherwise tests that use null data pointers wont find them.

--- c_VSIP_testing/gen_atest.sh
+++ c_VSIP_testing/gen_atest.sh
@@ -10,9 +10,9 @@ AFUNC=`eval echo $AFILE | sed '/\.h/!d' | sed 's/\.h/();/'`
 echo "/* atest.c */" >atest.c
 echo "/* $AFILE  */" >>atest.c
 echo "#include<stdio.h>\n#include<string.h>\n#include<vsip.h>\n" >>atest.c
-echo "#include\"$ATEST\"\n" >>atest.c
 echo "#define NDPTR_f ((vsip_scalar_f*)NULL)\n#define NDPTR_d ((vsip_scalar_d*)NULL)" >>atest.c
 echo "#define NDPTR_i ((vsip_scalar_i*)NULL)\n#define NDPTR_si ((vsip_scalar_si*)NULL)" >>atest.c
+echo "#include\"$ATEST\"\n" >>atest.c
 echo "int main(){\n\tvsip_init((void*)0);"  >>atest.c
 echo "\n/* function to test */\n\t$AFUNC\n" >>atest.c
 echo "\tvsip_finalize((void*)0); \n\treturn 0;\n} " >>atest.c
