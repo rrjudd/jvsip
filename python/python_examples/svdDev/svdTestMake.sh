8d8cdefcea368d1fa5adeb1b27159f1d3a786ff4
Randall R Judd <judd@RJ-MacBook-Air.local>
2014-02-01 15:46:01 -0800

    Cleaning up svdDev directory. Removing unwanted files.

--- python/python_examples/svdDev/svdTestMake.sh
+++
@@ -1,4 +0,0 @@
-cc -o test_svd_f test_svd_f.c svd_f.c -I$HOME/local/src/jvsip/include -L$HOME/local/src/jvsip/lib -lvsip
-cc -o test_svd_d test_svd_d.c svd_d.c -I$HOME/local/src/jvsip/include -L$HOME/local/src/jvsip/lib -lvsip
-cc -o test_csvd_f test_csvd_f.c csvd_f.c -I$HOME/local/src/jvsip/include -L$HOME/local/src/jvsip/lib -lvsip
-cc -o test_csvd_d test_csvd_d.c csvd_d.c -I$HOME/local/src/jvsip/include -L$HOME/local/src/jvsip/lib -lvsip
