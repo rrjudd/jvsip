6b30a95d6d59b8dcac183389f915ecb327d2fa5c
Randall R Judd <rrjudd@me.com>
2014-11-11 14:34:35 +1300

    Add example17 from TVCPP along with pyJvsip version.

---
+++ python/pyJvsip_example/example17/example17.py
@@ -0,0 +1,11 @@
+import pyJvsip as pjv
+from math import pi as pi
+L=50 # A length
+aBlock = pjv.Block('block_d',L)
+a=aBlock.bind(0,1,L).fill(0.0)
+b=a.empty.ramp(0.0,2.0*pi/float(L-1)).cos
+ab_bl = b.lgt(a)
+assert ab_bl.anytrue,'No true values in boolean vector.'
+ab_vi=ab_bl.indexbool
+pjv.gather(b,ab_vi,a.putlength(ab_vi.length))
+b.mprint('%.3f')
