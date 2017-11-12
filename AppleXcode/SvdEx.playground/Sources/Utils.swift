b93481699eed8ec1d42fd634924c06c7905d8e77
Randall Judd <rrjudd@me.com>
2017-11-12 06:58:25 -0800

    Initial commit

---
+++ AppleXcode/SvdEx.playground/Sources/Utils.swift
@@ -0,0 +1,18 @@
+import Foundation
+import SJVsip
+
+public func checkSvd(_ svd:Svd, against M: Matrix) -> Bool {
+    let chk = M.empty
+    chk.fill(Scalar(0.0))
+    let tmp = chk.newCopy
+    let U = svd.matU!
+    let Vt = svd.matV!.transview
+    let s = svd.vecS
+    copy(from: s, to: chk.diagview)
+    prod(chk, prod: Vt, resultsIn: tmp)
+    prod(U, prod: tmp, resultsIn:chk)
+    sub(chk, subtract: M, resultsIn: chk)
+    let aNumber = sumsqval(chk).reald/sumsqval(M).reald
+    print(aNumber)
+    return aNumber < 1E-10
+}
