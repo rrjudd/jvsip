1d62953c2f8fd5550f20884979891f678cda5243
Randall Judd <judd@RRJMacStudio.hsd1.or.comcast.net>
2026-07-29 14:41:24 -0700

    better installation files

--- python/jvsipNumpy/setup.py
+++ python/jvsipNumpy/setup.py
@@ -1,19 +1,28 @@
-#!/usr/bin/env python3
+#!python
 """
-setup.py file for numpy jvsip utilities module
+Python setup file. Creates JVSIP vsip module.
+Requirements:
+    C Compiler
+    python setuptools module needs to be downloaded
+    SWIG program installed (www.swig.org)
+    Need to have built the JVSIP vsip library (libvsip.a)
 """
-from setuptools import setup, Extension
-import numpy as np
-incld = np.get_include()
+from numpy import get_include
+npIncld = get_include()
 
+from setuptools import setup, Extension
+
+jvsipNumpyUtils_module = Extension('_jvsipNumpyUtils',
+                        sources=['jvsipNumpyUtils.i',
+                                 'numpyArrayCopies.c'],
+                        include_dirs=['../../c_VSIP_src','../../include','./',npIncld],
+                        library_dirs=['../../lib'],
+                        libraries=['vsip']
+                       )
 setup (name = 'jvsipNumpy',
        version = '0.1',
-       author  = "Randall Judd",
-       description = """Numpy Utilities for Use in pyJvsip""",
-       ext_modules = [Extension('_jvsipNumpyUtils',
-                                    sources=['./numpyArrayCopies.c',
-                                             './jvsipNumpyUtils_wrap.c'],
-                                    include_dirs=['./','../../../c_VSIP_src',incld]),
-                    ],
-       py_modules = ["jvsipNumpyUtils","jvsipNumpy"],
+       author      = "Randall Judd",
+       description = """jvsip numpy Extension Module""",
+       ext_modules = [jvsipNumpyUtils_module],
+       py_modules=['jvsipNumpy','jvsipNumpyUtils'],
     )
